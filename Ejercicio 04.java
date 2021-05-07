fde1c8a72e3d96fefa8c497192a60f6d4bbccf85
andre0305100 <>
2021-05-07 15:29:49 +0000

    actividad

---
+++ Ejercicio 04.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Ejer04{
+static Scanner input = new Scanner (System.in);
+public static void main (String[] args){
+  int edad;
+  int sexo;
+  int tipo_de_vacuna;
+
+System.out.println("¿cual es su edad?");
+edad=input.nextInt();
+
+System.out.println("cual es su genero?");
+sexo=input.nextInt();
+
